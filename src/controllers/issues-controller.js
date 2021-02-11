b587913a75d841e3873ad31cab470cc4b5af12bb
Ida Rosvall <ir222gn@student.lnu.se>
2021-02-11 10:36:58 +0100

    Succesfully fetch open issues from gitlab project

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -5,12 +5,14 @@
  * @version 1.0.0
  */
 
+import fetch from 'node-fetch'
+
 /**
  * Encapsulates the issues controller.
  */
 export class IssuesController {
   /**
-   * Displays the start page containing issues.
+   * Displays the start page containing open issues.
    *
    * @param {object} req - Express request object.
    * @param {object} res - Express response object.
@@ -18,7 +20,26 @@ export class IssuesController {
    */
   async index (req, res, next) {
     try {
-      res.render('issues/index')
+      let issues = await fetch(`${process.env.PROJECT_ISSUES_LINK}?state=opened`, {
+        headers: {
+          authorization: `bearer ${process.env.ACCESS_TOKEN}`
+        }
+      })
+      issues = await issues.json()
+
+      const viewData = {
+        snippets: issues
+          .map(issue => ({
+            id: issue.id,
+            title: issue.title,
+            description: issue.description,
+            author: issue.author.name,
+            avatar: issue.author.avatar_url,
+            createdAt: issue.created_at.slice(0, 10)
+          }))
+      }
+
+      res.render('issues/index', { viewData })
     } catch (error) {
       next(error)
     }
