43165451bb2821c11a342d3d6e0fa4501565bbcb
Ida Rosvall <ir222gn@student.lnu.se>
2021-02-18 16:25:25 +0100

    Make issues update in real time on the client side

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '../socket.io/socket.io.js'
 const issueTemplate = document.querySelector('#issue-template')
 
 const baseURL = document.querySelector('base').getAttribute('href')
+const issuesContainer = document.querySelector('#issues-container')
 
 // If issueTemplate is not present on the page, just ignore and do not listen for tasks
 if (issueTemplate) {
@@ -12,12 +13,15 @@ if (issueTemplate) {
   // Create a socket connection using Socket.io
   const socket = window.io({ path: `${baseURL}socket.io` })
 
-  // Listen for message "issue" from the server
+  // Listen to messages from the server.
   socket.on('newIssue', arg => {
     const issue = createIssue(arg)
-
-    const taskList = document.querySelector('#issues-container')
-    taskList.insertBefore(issue, taskList.firstChild)
+    issuesContainer.insertBefore(issue, issuesContainer.firstChild)
+  })
+  socket.on('updateIssue', arg => {
+    document.getElementById(arg.id).remove()
+    const issue = createIssue(arg)
+    insertIssue(issuesContainer, issue)
   })
 
   /**
@@ -30,8 +34,32 @@ if (issueTemplate) {
     const issueString = hbsTemplate(arg)
     const issue = document.createElement('div')
     issue.classList.add('issue')
+    issue.setAttribute('id', arg.id)
     issue.innerHTML = issueString
 
     return issue
+  }
+
+  /**
+   * Inserts an issue in the document in ID order.
+   *
+   * @param {HTMLElement} container - The element for the issue to be inserted in.
+   * @param {HTMLElement} issue - The issue.
+   */
+  function insertIssue (container, issue) {
+    let issues = document.querySelectorAll('.issue')
+
+    if (issues.lenght === 0) {
+      container.appendChild(issue)
+    } else {
+      issues = Array.from(issues)
+
+      for (const element of issues) {
+        if (element.id < issue.id) {
+          container.insertBefore(issue, element)
+          return
+        }
+      }
+    }
   }
 }
